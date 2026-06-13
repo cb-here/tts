30d5f528c1a418ad991424a3a9078d993eafe6e0
cb-here <cbhere42@gmail.com>
2026-06-13 21:20:57 +0530

    update

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  
+    allow_origins=["http://localhost:5173","https://suno-story.vercel.app"],  
     allow_methods=["*"],
     allow_headers=["*"],
 )
